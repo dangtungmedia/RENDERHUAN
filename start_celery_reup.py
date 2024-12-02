1638bc47701e33a3205b1f2eb34c866f1cf53e33
dangtung <dangtungmedia@gmail.com>
2024-12-02 22:04:37 +0700

    update content  oki

--- start_celery_reup.py
+++ start_celery_reup.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import psutil
 
 def get_public_ip():
     try:
@@ -14,21 +13,11 @@ def get_public_ip():
     except Exception as e:
         print(f"Error getting public IP: {e}")
         return None
-    
-
-
-        # Số lõi logic (bao gồm cả những lõi ảo nếu có Hyper-Threading)
-        logical_cores = psutil.cpu_count(logical=True)
-        # Số lõi vật lý
-        physical_cores = psutil.cpu_count(logical=False)
-
-    
-    
 
 if __name__ == "__main__":
     # Lấy địa chỉ IP public
     ip_address = get_public_ip()
     # Chạy Celery worker với 2 worker và queue render_video
-    os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Reup --concurrency={physical_cores -1} -Q render_video_reupload")
+    os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Reup --concurrency=1 -Q render_video_reupload")
     
     
