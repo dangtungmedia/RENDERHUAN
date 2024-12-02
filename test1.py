446523acd0dbcd80df66a6568aeb06674682a05f
dangtung <dangtungmedia@gmail.com>
2024-12-02 16:07:57 +0700

    update lan 2

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-from random_video_effect import random_video_effect_cython
+# from random_video_effect import random_video_effect_cython
 
 image_path = "3.png"  # Set image path
 out_path = "caches.mp4"  # Set output path
@@ -13,4 +13,14 @@ height = 1080  # Video height
 # create_zoom_out_video(image_path, out_path, duration, fps, width, height)
 # create_zoom_in_video_with_background(image_path, out_path, duration, fps, width, height)
 # create_zoom_out_video_with_background(image_path, out_path, duration, fps, width, height)
-random_video_effect_cython(image_path, out_path, duration, fps, width, height)
+# random_video_effect_cython(image_path, out_path, duration, fps, width, height)
+
+
+import psutil
+
+# Số lõi logic (bao gồm cả những lõi ảo nếu có Hyper-Threading)
+logical_cores = psutil.cpu_count(logical=True)
+# Số lõi vật lý
+physical_cores = psutil.cpu_count(logical=False)
+
+print(f"Số lõi vật lý: {physical_cores}")
