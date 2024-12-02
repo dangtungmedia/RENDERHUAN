ec9003dd51da5e5e7ee1a334f048bfad0a21e1a2
dangtung <dangtungmedia@gmail.com>
2024-12-02 16:37:23 +0700

    update code

--- start_celery_content.py
+++ start_celery_content.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import psutil
 
 def get_public_ip():
     try:
@@ -14,11 +15,21 @@ def get_public_ip():
         print(f"Error getting public IP: {e}")
         return None
 
+def get_cpu_cores():
+    # Số lõi logic (bao gồm cả những lõi ảo nếu có Hyper-Threading)
+    logical_cores = psutil.cpu_count(logical=True)
+    # Số lõi vật lý
+    physical_cores = psutil.cpu_count(logical=False)
+    return physical_cores, logical_cores
+
 if __name__ == "__main__":
     # Lấy địa chỉ IP public
     ip_address = get_public_ip()
-    
-    # Chạy Celery worker với 2 worker và queue render_video
-    os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Content --concurrency=3 -Q render_video_content")
-    
-    
+    if ip_address:
+        # Lấy số lượng lõi CPU vật lý
+        physical_cores, logical_cores = get_cpu_cores()
+
+        # Chạy Celery worker với số lượng worker là physical_cores - 1 (bớt 1 để tránh overload)
+        os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Reup --concurrency={physical_cores - 1} -Q render_video_reupload")
+    else:
+        print("Không thể lấy địa chỉ IP public.")
--- start_celery_reup.py
+++ start_celery_reup.py
@@ -14,22 +14,21 @@ def get_public_ip():
     except Exception as e:
         print(f"Error getting public IP: {e}")
         return None
+    
 
-def get_cpu_cores():
-    # Số lõi logic (bao gồm cả những lõi ảo nếu có Hyper-Threading)
-    logical_cores = psutil.cpu_count(logical=True)
-    # Số lõi vật lý
-    physical_cores = psutil.cpu_count(logical=False)
-    return physical_cores, logical_cores
+
+        # Số lõi logic (bao gồm cả những lõi ảo nếu có Hyper-Threading)
+        logical_cores = psutil.cpu_count(logical=True)
+        # Số lõi vật lý
+        physical_cores = psutil.cpu_count(logical=False)
+
+    
+    
 
 if __name__ == "__main__":
     # Lấy địa chỉ IP public
     ip_address = get_public_ip()
-    if ip_address:
-        # Lấy số lượng lõi CPU vật lý
-        physical_cores, logical_cores = get_cpu_cores()
-
-        # Chạy Celery worker với số lượng worker là physical_cores - 1 (bớt 1 để tránh overload)
-        os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Reup --concurrency={physical_cores - 1} -Q render_video_reupload")
-    else:
-        print("Không thể lấy địa chỉ IP public.")
+    # Chạy Celery worker với 2 worker và queue render_video
+    os.system(f"celery -A celeryworker worker -l INFO --hostname={ip_address}-Reup --concurrency={physical_cores -1} -Q render_video_reupload")
+    
+    
